9bad5adf01bbbee8c738cef998dd377442dafc91
Christian Brolin <brolin@gmail.com>
2021-12-10 21:38:03 +0100

    Solve AoC 01.

---
+++ src/aoc01.hs
@@ -0,0 +1,24 @@
+import Data.Char (isDigit, isSpace)
+import System.Environment (getArgs)
+import qualified Text.ParserCombinators.ReadP as P
+
+main = do
+  input <- parse numbers1 <$> (getArgs >>= readFile . head)
+  p 'a' $ nInc input
+  p 'b' $ nInc $ map add3 $ zip3 input (tail input) (tail $ tail input)
+  where
+    p c result = putStrLn $ c : ": " ++ show result
+    nInc input = length $ filter (uncurry (<)) $ zip input $ tail input
+    add3 (a, b, c) = a+b+c
+
+parse :: P.ReadP a -> String -> a
+parse p = head . fmap fst . P.readP_to_S (p <* P.skipSpaces <* P.eof)
+
+numbers1 :: P.ReadP [Int]
+numbers1 = P.sepBy1 number space
+
+number :: P.ReadP Int
+number = read <$> P.many1 (P.satisfy isDigit)
+
+space :: P.ReadP String
+space = P.munch1 isSpace
