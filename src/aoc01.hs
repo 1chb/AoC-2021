4407e0b0b78924628c03b8911b430fa5a710bb0d
Christian Brolin <brolin@gmail.com>
2021-12-10 21:44:22 +0100

    Refactor AoC 01.

--- src/aoc01.hs
+++ src/aoc01.hs
@@ -5,11 +5,11 @@ import qualified Text.ParserCombinators.ReadP as P
 main = do
   input <- parse numbers1 <$> (getArgs >>= readFile . head)
   p 'a' $ nInc input
-  p 'b' $ nInc $ map add3 $ zip3 input (tail input) (tail $ tail input)
+  p 'b' $ nInc $ zipWith3 add3 input (tail input) (tail $ tail input)
   where
     p c result = putStrLn $ c : ": " ++ show result
     nInc input = length $ filter (uncurry (<)) $ zip input $ tail input
-    add3 (a, b, c) = a+b+c
+    add3 a b c = a+b+c
 
 parse :: P.ReadP a -> String -> a
 parse p = head . fmap fst . P.readP_to_S (p <* P.skipSpaces <* P.eof)
