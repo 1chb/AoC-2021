057c533bbac8941986decbe43c47e5a652aa4283
Christian Brolin <brolin@gmail.com>
2021-12-12 00:38:18 +0100

    Refactor AoC 03.

--- src/aoc03.hs
+++ src/aoc03.hs
@@ -3,21 +3,22 @@ import System.Environment (getArgs)
 main = do
   input <- lines <$> (getArgs >>= readFile . head)
   let bss = map readDigits input
-  let gammaBits = bits (length bss) $ foldr1 sums bss
+  let gammaBits = bits $ foldr1 addPairs $ map (1:) bss
   let epsilonBits = map (1-) gammaBits
   p 'a' $ value gammaBits * value epsilonBits
-  p 'b' $ value (find (>=) bss) * value (find (<) bss)
+  let (oxygen, carbonDioxide) = (findRating (>=) bss, findRating (<) bss)
+  p 'b' $ value oxygen * value carbonDioxide
   where
     p c result = putStrLn $ c : ": " ++ show result
 
 readDigits :: String -> [Int]
 readDigits line = map (read . (:"")) line
 
-sums :: [Int] -> [Int] -> [Int]
-sums = zipWith (+)
+addPairs :: [Int] -> [Int] -> [Int]
+addPairs = zipWith (+)
 
-bits :: Int -> [Int] -> [Int]
-bits s xs = map comp xs
+bits :: [Int] -> [Int]
+bits (s:xs) = map comp xs
   where
     comp x | 2*x < s = 0
            | otherwise = 1
@@ -25,16 +26,15 @@ bits s xs = map comp xs
 value :: [Int] -> Int
 value = foldl (\v b -> 2*v+b) 0
 
-find :: (Int -> Int -> Bool) -> [[Int]] -> [Int]
-find cmp bss = go1st $ zip bss bss
+findRating :: (Int -> Int -> Bool) -> [[Int]] -> [Int]
+findRating cmp bss = go1st $ zip bss bss
   where
     go1st [(_s, bs)] = bs
     go1st bs2s
       = go1st
-      $ map (\(_:as, bs) -> (as, bs))
-      $ filter (\(a:_, bs) -> a == d) bs2s
+      $ map    (\(_:as, bs) -> (as, bs))
+      $ filter (\(a:__, bs) -> a == d) bs2s
       where
         s = length bs2s
         d = if (2*k) `cmp` s then 1 else 0
         k = sum $ map (head . fst) bs2s
-        
